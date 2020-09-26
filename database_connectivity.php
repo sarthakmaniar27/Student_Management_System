61da3d9dc5f8d226f2a9a7d44e2fecd4f686fd72
Sarthakmaniar27 <techno.codeuseme@gmail.com>
2020-09-26 20:33:01 +0530

    5th commit

--- database_connectivity.php
+++ database_connectivity.php
@@ -3,11 +3,13 @@
 //$conn = mysqli_connect('localhost','root','','Student_Management_System');
 //$conn1 = mysqli_connect('localhost','root','','Replicated_sms');
 
-//$conn = mysqli_connect('localhost','root','','Student_Management_System');
-//$conn = mysqli_connect('sql7.freemysqlhosting.net','sql7367470','zn9LppXPCD','sql7367470');
-$conn = mysqli_connect('sql3.freemysqlhosting.net','sql3367473','9rIezusJsc','sql3367473');
-$conn1 = mysqli_connect('sql3.freemysqlhosting.net','sql3367473','9rIezusJsc','sql3367473');
-$conn2 = mysqli_connect('sql12.freemysqlhosting.net','sql12366578','BW7pMbPgJ2','sql12366578');
+
+//$conn = mysqli_connect('sql3.freemysqlhosting.net','sql3367473','9rIezusJsc','sql3367473');
+//$conn1 = mysqli_connect('sql3.freemysqlhosting.net','sql3367473','9rIezusJsc','sql3367473');
+
+$conn = mysqli_connect('sql12.freemysqlhosting.net','sql12367483','7IpCVZLSe1','sql12367483'); //bhargav
+$conn1 = mysqli_connect('sql12.freemysqlhosting.net','sql12367484','zZiShGbvqa','sql12367484');   //kaustubh
+$conn2 = mysqli_connect('sql12.freemysqlhosting.net','sql12366578','BW7pMbPgJ2','sql12366578'); //sarthak
 if (!$conn && !$conn1 && !$conn2) {
  die("Connection failed: " . mysqli_connect_error());
 }
